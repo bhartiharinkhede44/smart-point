ba1bedcdd3cd728b0511cc344922d6307ab79417
bhartiharinkhede44 <bhartiharinkhede44@gmail.com>
2023-10-28 19:09:01 +0530

    signup and login and find all user

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+import {model,Schema} from 'mongoose'
+
+const UserSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    mobile:{
+        type:String,
+        default:0,
+        unique:true,
+        required:true
+    },
+    address:{
+        type:String,
+
+    },
+    gender:{
+        type:String,
+        default:"not to say ......"
+    }
+})
+
+const User = model('User',UserSchema)
+
+export default User
